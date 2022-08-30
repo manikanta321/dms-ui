a4e6d55a0ef391fc8588910e2a79132f0b1cdb59
Manikanta_dk <Manikanta.dk@smartgig.tech>
2022-08-30 16:59:35 +0530

    new commit

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { SignUpComponent } from './component/sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
 import { DashbordComponent } from './shared-component/dashbord/dashbord.component';
 import { SidenavBarComponent } from './shared-component/sidenav-bar/sidenav-bar.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,8 @@ import { SidenavBarComponent } from './shared-component/sidenav-bar/sidenav-bar.
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    MatSlideToggleModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]

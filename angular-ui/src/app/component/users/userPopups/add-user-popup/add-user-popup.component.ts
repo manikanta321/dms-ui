018319240974871d0d89743f3948da6f8724dcce
revathi <revathi.k@infinitylearn.com>
2022-09-01 16:44:29 +0530

    revathi popup pags

---
+++ angular-ui/src/app/component/users/userPopups/add-user-popup/add-user-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-user-popup',
+  templateUrl: './add-user-popup.component.html',
+  styleUrls: ['./add-user-popup.component.css']
+})
+export class AddUserPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular-ui/src/app/component/users/users.component.ts
+++ angular-ui/src/app/component/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AddUserPopupComponent } from './userPopups/add-user-popup/add-user-popup.component';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,11 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog,
+    private router: Router,) { }
 
   ngOnInit(): void {
   }
   addUser(){
-
+   this.dialog.open( AddUserPopupComponent );
   }
 }

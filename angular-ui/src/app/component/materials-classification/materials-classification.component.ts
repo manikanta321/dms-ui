82df4d96e5c77d9b3984358f074ace663d517352
revathi <revathi.k@infinitylearn.com>
2022-09-05 16:00:22 +0530

    revathi changed clssf

--- angular-ui/src/app/component/materials-classification/materials-classification.component.ts
+++ angular-ui/src/app/component/materials-classification/materials-classification.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./materials-classification.component.css']
 })
 export class MaterialsClassificationComponent implements OnInit {
-
+  clData: string[] = ['Type TP 1', 'Type TP 2', 'Type TP 3','Type TP 4'];
+  subcat: string[] = ['sub category', 'sub category 2',];
   constructor() { }
 
   ngOnInit(): void {

18a0230f3fade2c280248ca2eda134eef549b8a2
revathi <revathi.k@infinitylearn.com>
2022-09-26 12:23:26 +0530

    revathi added dealer routing

--- angular-ui/src/app/association/association.component.ts
+++ angular-ui/src/app/association/association.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AssociationTargetPopupComponent } from './association-target-popup/association-target-popup.component';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-association',
@@ -9,38 +10,47 @@ import { AssociationTargetPopupComponent } from './association-target-popup/asso
   styleUrls: ['./association.component.css']
 })
 export class AssociationComponent implements OnInit {
-  selectedCar: number | undefined;
-  toppings = new FormControl('');
-  toppingList: string[] = ['Value1', 'Value2', 'Value3'];
-  selectedPeople = [];
-  cities: any[] = [
-    { value: 1, text: "Vilnius" },
-    { value: 2, text: "Kaunas" },
-    { value: 3, text: "Pavilnys (Disabled)" },
-    { value: 4, text: "Pabradė" }
-  ];
-  showUploadButton:boolean = false;
-  bulkBtnClicked:boolean = false;
-  // selectedCar: number;
+  toppings1 = new FormControl('');
+  toppingList1:  any= [];
   
-  
-  constructor(public dialog: MatDialog,) { }
+  constructor(public dialog: MatDialog,
+    private user:UserService,) { }
 
-    cars = [
-        { id: 1, name: 'Volvo' },
-        { id: 2, name: 'Saab' },
-        { id: 3, name: 'Opel' },
-        { id: 4, name: 'Audi' },
-    ];
+   
 
   ngOnInit(): void {
   }
-
-  targetSetModal(){
-    const dialogRef =  this.dialog.open( AssociationTargetPopupComponent, { panelClass: 'custom-dialog-container' });
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
+  statusItems(){
+    this.user.getstatusDeatils().subscribe((res: any) => {
+        
+      let localdata=res.response;
+  
+  
+      this.toppingList1 = localdata.map((data: { statusId: any; statusname: any; }) => {
+        return {status_id: data.statusId, status_name: data.statusname };
+      });
+  
+      if (!this.toppingList1?.length) {
+        this.toppingList1 = localdata.map((status: { statusname: any; }) => {
+          return status.statusname;
+        });
+      }
+      this.toppingList1.push()
+      // this.toppingList = res.response;
+      this.toppings1 = new FormControl(this.toppingList1);
+  
+      console.log('status',this.toppingList1)
+  
+  
+  
+  
     });
   }
+  // targetSetModal(){
+  //   const dialogRef =  this.dialog.open( AssociationTargetPopupComponent, { panelClass: 'custom-dialog-container' });
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log(`Dialog result: ${result}`);
+  //   });
+  // }
 
 }

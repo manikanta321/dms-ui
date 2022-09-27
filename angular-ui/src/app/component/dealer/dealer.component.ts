8fec83a3677de5d29a40062a24be711955411b5b
revathi <revathi.k@infinitylearn.com>
2022-09-27 09:54:54 +0530

    revathi dealer ts file changes

--- angular-ui/src/app/component/dealer/dealer.component.ts
+++ angular-ui/src/app/component/dealer/dealer.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-dealer',
   templateUrl: './dealer.component.html',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DealerComponent implements OnInit {
   sideBarOpen = true;
-  constructor() { }
-
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
+  constructor(private observer: BreakpointObserver,) { }
+  ngAfterViewInit() {
+    // this.dataSource.sort = this.sort;
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
   ngOnInit(): void {
   }
-
+  sideBarToggler(){
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }

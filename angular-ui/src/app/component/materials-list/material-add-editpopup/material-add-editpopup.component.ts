d4793c4b8a1752b00497e06668d1b258a90b0d33
Sonam0801 <sonam.khadayate@smartgig.tech>
2022-10-17 10:28:50 +0530

    worked on add materials popup screen

--- angular-ui/src/app/component/materials-list/material-add-editpopup/material-add-editpopup.component.ts
+++ angular-ui/src/app/component/materials-list/material-add-editpopup/material-add-editpopup.component.ts
@@ -50,7 +50,15 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
   dropdownSettings3: IDropdownSettings = {};
   toppingList2:  any= []; 
   toppingList3:  any= [];
-   
+  countryname: string[] = ['Malaysia (71/126)', 'India (178/178)','Philipines (0/135)'];
+  statename: string[] = ['Johor(0/42)', 'Kedah(36/36','Perak(14/26)','Penang(21/22)'];
+  regionname: string[] = ['North(4/4)', 'South(8/8)', 'East(6/6)','West(3/4)'];
+  cityname:string[] =['George town','Balik Pulau','Batu Refringi','Teluk Bahang'];
+  toprint:boolean=false;
+  addButton:boolean =false;
+  removelist:boolean =false;
+  toggle:boolean=true;
+  selectedItem = null;
   constructor(private _formBuilder: FormBuilder) {}
    firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
    secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
@@ -66,7 +74,27 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
   onTypeAll(items: any) {
     console.log('onSelectAll', items);
   }
+  cname1(cname:string,i:any){
+    
+    if(cname=='Eectronics'){
+      // this.Sname= ['mobile', 'earphone','mouse'];
+    }
+    if(cname!='Eectronics'){
+      // this.Sname= ['fan','fridge'];
+    
 
+    }
+  }
+  printvalue(valueofprint:boolean){
+    this.toprint=valueofprint;
+  }
+  addCategory(){
+    this.addButton =true;
+  }
+  
+    onClick(item) {
+    this.selectedItem = item;
+  }
  }
 
  

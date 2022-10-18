efcaff16ce6fa4afa11bf35f3cdac37d0e5cf9d2
revathi <revathi.k@infinitylearn.com>
2022-10-18 14:44:51 +0530

    changes in the unit measure material

--- angular-ui/src/app/component/other-masters/other-masters.component.ts
+++ angular-ui/src/app/component/other-masters/other-masters.component.ts
@@ -1,52 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { AddUserPopupComponent } from '../users/userPopups/add-user-popup/add-user-popup.component';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import {AfterViewInit, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import {FormControl} from '@angular/forms';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  displaycode:any;
-   status:any;
-}
-export interface PeriodicElement1 {
-  name: string;
-  position: number;
-  displayunit:any;
-   status:any;
-   rate:any;
-   effectivedate:any;
-   standerdcur:any;
-}
-
-
-const ELEMENT_DATA: PeriodicElement[] = [
- 
-  {position: 2, name: 'Hydrogen', displaycode: 1.0079, status:'Delete'},
-  {position: 3, name: 'Hydrogen', displaycode: 1.0079, status:'active'},
-  {position: 4, name: 'Hydrogen', displaycode: 1.0079, status:'active'},
-  {position: 5, name: 'Hydrogen', displaycode: 1.0079, status:'active'},
-  {position: 6, name: 'Hydrogen', displaycode: 1.0079, status:'Delete'},
-
-];
-
-const ELEMENT_DATA1: PeriodicElement1[] = [
-  {position: 1, name: 'Hydrogen', displayunit: 1.0079, rate:12,effectivedate:123,standerdcur:123, status:'active'}, 
-  {position: 1, name: 'Hydrogen', displayunit: 1.0079, rate:12,effectivedate:123,standerdcur:123, status:'active'},
-  {position: 1, name: 'Hydrogen', displayunit: 1.0079, rate:12,effectivedate:123,standerdcur:123, status:'active'},
-  {position: 1, name: 'Hydrogen', displayunit: 1.0079, rate:12,effectivedate:123,standerdcur:123, status:'active'},
-];
-import { EditPopupComponent } from '../users/userPopups/edit-popup/edit-popup.component';
-import { UomPopupComponent } from '../users/userPopups/uom-popup/uom-popup.component';
-import { EditUomPopupComponent } from '../users/userPopups/edit-uom-popup/edit-uom-popup.component';
-import { AddTaxTemplateComponent } from '../users/userPopups/add-tax-template/add-tax-template.component';
-import { AddcurrencyComponent } from '../users/userPopups/addcurrency/addcurrency.component';
-import { EditTaxTemplateComponent } from '../users/userPopups/edit-tax-template/edit-tax-template.component';
+import { Component, OnInit,  } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import {AfterViewInit, ViewChild} from '@angular/core';
+import { Sort, MatSort, SortDirection } from '@angular/material/sort';
 import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
@@ -55,24 +10,14 @@ import { BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./other-masters.component.css']
 })
 export class OtherMastersComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'status','edit'];
-  displayedColumns1: string[] = ['position', 'name', 'displayunit','rate','effectivedate','standerdcur','status','edit'];
-
-  dataSource = ELEMENT_DATA;
-  dataSource1=ELEMENT_DATA1;
-  toppings = new FormControl('');
-  toppingList: string[] = ['Active', 'Inactive'];
-  sideBarOpen = true;
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
-  constructor(public dialog: MatDialog,
-    private observer: BreakpointObserver,
-    private router: Router,) { }
 
-  ngOnInit(): void {
- 
-  }
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort;
+
   ngAfterViewInit() {
+    //this.dataSource.sort = this.sort;
     this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
       if (res.matches) {
         this.sidenav.mode = 'over';
@@ -83,34 +28,16 @@ export class OtherMastersComponent implements OnInit {
       }
     });
   }
-  sideBarToggler(){
-    this.sideBarOpen = !this.sideBarOpen;
+  constructor(
+    private observer: BreakpointObserver,) { }
+
+  ngOnInit(): void {
+ 
   }
-  
-  addUser(){
-   this.dialog.open( AddUserPopupComponent,  { height: '580px', });
+  ToggleSideNav(value:any){
+    this.sidenav.toggle()
   }
-  editUser(){
-    this.dialog.open( EditPopupComponent,);
-  }
-  AddUomPopup(){
-    this.dialog.open(UomPopupComponent,);
-  }
-  EditUomPopup(){
-    this.dialog.open( EditUomPopupComponent,);
-  }
-  addtaxTempl(){
-    this.dialog.open( AddTaxTemplateComponent,);
-  }
-  editTaxTemp(){
-    this.dialog.open(EditTaxTemplateComponent,);
-  }
-  addCurrency(){
-    this.dialog.open( AddcurrencyComponent);
-  }
-  unitMeasure(){
-    this.router.navigate(['unit-measure']);
-  }
+
 }
 
 

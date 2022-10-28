828ddcc574fe530793dbde302ad407dd901dce3c
ShivamSG <shivam.c@smartgig.tech>
2022-10-28 12:10:18 +0530

    Material list integration nd Ui issues solve

--- angular-ui/src/app/component/materials-list/materials-list.component.ts
+++ angular-ui/src/app/component/materials-list/materials-list.component.ts
@@ -8,7 +8,7 @@ import { UserService } from 'src/app/services/user.service';
 import { GuiColumn, GuiColumnMenu, GuiPaging, GuiPagingDisplay, GuiSearching, GuiSorting } from '@generic-ui/ngx-grid';
 import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';  
-
+import { MaterialListService } from 'src/app/services/material-list.service';
   
 // import { ButtonRendererComponent } from './renderer/button-renderer.component';
 
@@ -48,7 +48,36 @@ export class MaterialsListComponent implements OnInit {
   subCategorySelection =false;
   typeSelection =false;
   productSelection =false;
+  coutCatagory:any;
+  catgname:any;
+  catNamee:any;
+  toppings: any= [];
+  topping1:any=[];
+  itemId:any;
+  catagoryName:any;
+  subCatty:any=[];
+  prodItem:any;
+  itemId1:any;
+  types:any;
+  typss:any;
+  Product:any;
+  statusList:any;
+  topping2:any =[];
+  ProductList:any=[];
   toppingList: any= [];
+  catergory:any=[];
+  ProdData:any=[];
+  sub_category:any=[];
+  typeI:any=[];
+  searchText:any;
+  statusTypes:any=[];
+  productID:any =[];
+  statusData:any=[];
+  catArray:any[] = [];
+  prodArray:any[] =[];
+  catagData:any =[];
+
+  prodData:any =[];
   toppingList1:  any= [];
   toppingList2:  any= [];
   toppingList3:  any= [];
@@ -60,16 +89,14 @@ export class MaterialsListComponent implements OnInit {
   dropdownSettings4: IDropdownSettings = {};
   constructor(public dialog: MatDialog,
     private user:UserService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private materialList:MaterialListService) { }
 
   ngOnInit(): void {
-    this.toppingList = [
-      { CategoryId: 1, CategoryName: 'Mumbai' },
-      { CategoryId: 2, CategoryName: 'Bangaluru' },
-      { CategoryId: 3, CategoryName: 'Pune' },
-      { CategoryId: 4, CategoryName: 'Navsari' },
-      { CategoryId: 5, CategoryName: 'New Delhi' }
-    ];
+    this.getMaterialList();
+    this.getclassification();
+    this.getProduct();
+    this.getStatusList();
     this.toppingList1 = [
       { statusId: 1, statusName: 'Mumbai' },
       { statusId: 2, statusName: 'Bangaluru' },
@@ -77,36 +104,15 @@ export class MaterialsListComponent implements OnInit {
       { statusId: 4, statusName: 'Navsari' },
       { statusId: 5, statusName: 'New Delhi' }
     ];
-    this.toppingList3 = [
-      { typeId: 1, typeText: 'Mumbai' },
-      { typeId: 2, typeText: 'Bangaluru' },
-      { typeId: 3, typeText: 'Pune' },
-      { typeId: 4, typeText: 'Navsari' },
-      { typeId: 5, typeText: 'New Delhi' }
-    ];
-    this.toppingList4 = [
-      {  productId: 1,  productText: 'Mumbai' },
-      {  productId: 2, productText: 'Bangaluru' },
-      {  productId: 3, productText: 'Pune' },
-      {  productId: 4, productText: 'Navsari' },
-      {  productId: 5, productText: 'New Delhi' }
-    ];
-    this.toppingList2 = [
-      {  subCategoryId: 1,  subCategoryText: 'Mumbai' },
-      {  subCategoryId: 2, subCategoryText: 'Bangaluru' },
-      {  subCategoryId: 3, subCategoryText: 'Pune' },
-      {  subCategoryId: 4, subCategoryText: 'Navsari' },
-      {  subCategoryId: 5, subCategoryText: 'New Delhi' }
-    ];
     this.selectedItems = [];
     this.selectedStatus =[];
   this.dropdownSettings = {
     singleSelection: false,
-    idField: 'CategoryId',
-    textField: 'CategoryName',
+    idField: 'catId',
+    textField: 'catName',
     selectAllText: 'Select All',
     unSelectAllText: 'UnSelect All',
-    itemsShowLimit: 2,
+    itemsShowLimit: 1,
     allowSearchFilter: this.ShowFilter
 };
 this.dropdownSettings1 = {
@@ -115,34 +121,34 @@ this.dropdownSettings1 = {
   textField: 'statusName',
   selectAllText: 'Select All',
   unSelectAllText: 'UnSelect All',
-  itemsShowLimit: 2,
+  itemsShowLimit: 1,
   allowSearchFilter: this.StatusFilter
 };
 this.dropdownSettings2 = {
   singleSelection: false,
-  idField: 'subCategoryId',
-  textField: 'subCategoryText',
+  idField: 'subCatId',
+  textField: 'subCatName',
   selectAllText: 'Select All',
   unSelectAllText: 'UnSelect All',
-  itemsShowLimit: 2,
+  itemsShowLimit: 1,
   allowSearchFilter: this.subCategoryFilter
 };
 this.dropdownSettings3 = {
   singleSelection: false,
   idField: 'typeId',
-  textField: 'typeText',
+  textField: 'typeName',
   selectAllText: 'Select All',
   unSelectAllText: 'UnSelect All',
-  itemsShowLimit: 2,
+  itemsShowLimit: 1,
   allowSearchFilter: this.typeFilter
 };
 this.dropdownSettings4 = {
   singleSelection: false,
-  idField: 'productId',
-  textField: 'productText',
+  idField: 'productGroupId',
+  textField: 'productGroupName',
   selectAllText: 'Select All',
   unSelectAllText: 'UnSelect All',
-  itemsShowLimit: 2,
+  itemsShowLimit: 1,
   allowSearchFilter: this.productFilter
 };
 this.myForm = this.fb.group({
@@ -161,21 +167,407 @@ this.subCategory = this.fb.group({
       product: [this.selectedItems]
       });
   }
+  getclassification(){
   
+    this.materialList.getclassification().subscribe((res)=>{
+      let data=res.response;
+      this.coutCatagory=res.totalRecords;
+      this.catgname=data.allOtherCats;
+      let dataCat = data.allOtherCats;
+      this.toppings = new FormControl(this.catgname);
+      // this.catNamee = this.catgname.catName;
+      console.log("materialList",this.materialList);
+      console.log("coutCategory",this.coutCatagory);
+      console.log("this.catgname",this.catgname);
+      console.log("this.catnamee",this.catNamee);
+      this.catagData = dataCat.map((data: { catId: any; catName: any; }) => {
+        return { catId: data.catId, roleName: data.catName };
+      });
+  
+      if (!this.catagData?.length) {
+        this.catagData = dataCat.map((product: { designationName: any; }) => {
+          return product.designationName;
+        });
+      }
+      this.catagData.push()
+     this.catagData.forEach(element=>{
+     return   this.catArray.push(element.catId);
+          // alert(this.catArray);
+  
+      })
+     })
+      }
+    getProduct(){
+      this.materialList.getProduct().subscribe((res)=>{
+        let data=res.response;
+        let dataProd =res.response
+        console.log("Product Data",data);
+        this.Product = data;
+        this.ProductList = new FormControl(this.Product);
+        this.prodData = dataProd.map((data: { productGroupId: any; productGroupName: any; }) => {
+          return { productGroupId: data.productGroupId, productGroupName: data.productGroupName };
+        });
+    
+        if (!this.prodData?.length) {
+          this.prodData = dataProd.map((category: { designationName: any; }) => {
+            return category.designationName;
+          });
+        }
+        this.prodData.push()
+       this.prodData.forEach(element=>{
+       return   this.prodArray.push(element.productGroupId);
+            // alert(this.catArray);
+    
+        })
+       })
+      }
+      getStatusList(){
+        // this.statusTypes.push(item.statusId);
+        this.materialList.getStatusList().subscribe((res)=>{
+          let data=res.response;
+          this.statusList = data;
+          console.log("StatusList",this.statusList);
+          this.statusData = new FormControl(this.statusList);
+         })
+      }
+      getMaterialList(){
+        const data={
+          Cat:[],
+          Sub_Cat:[],
+          type:[],
+          product:[],
+          status:[],
+          Search:""
+        }
+          this.materialList.getMaterialList(data).subscribe((res) => {
+          console.log("Material List",res);
+        this.rowData5 = res.response;
+        console.log('tableDaaaata', this.rowData5 )
+        if (this.rowData5.length >= 1) {
+        this.catgname.forEach((element: { [x: string]: any; }) => {
+        if (element['status']=='Active'){
+    }
+        else{
+          element['isActive']=='Inactive'
+    
+        }
+    console.log('element',element['isActive'])
+        });
+      }
+    
+        console.log('row data',this.catgname)
+    
+      });
+    }
+    onSearchChange($event:any , anything?:any){
+      const { target } = $event;
+      this.searchText=target.value;
+      const data={
+        Cat:this.catergory,
+        Sub_Cat:this.sub_category,
+        type:this.typeI,
+        product:this.productID,
+        status:this.statusTypes,
+        Search:this.searchText
+      }
+      this.materialList.getMaterialList(data).subscribe((res) => {     
+        this.rowData5 = res.response;
+      });
+  
+    }
   onItemSelect(item: any) {
-    console.log(item);
+    // this.selectedItem = item;
+    this.catergory.push(item.catId);
+    console.log('item Subcatty',item)
+    
+     this.itemId=item.catId;
+     this.catagoryName=item.catName;
+    this.materialList.onclickcat(this.itemId).subscribe((res)=>{
+      let subcaty = res.response;
+    console.log("responseeee",subcaty);
+this.subCatty = subcaty.allOtherSubCAts;
+console.log("SubCategory",this.subCatty);
+this.topping1 = new FormControl(this.subCatty);
+    });
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
+  }
+  onItemDeSelect(item: any) {
+
+    this.catergory.forEach((element,index)=>{
+      if(element==item.catId)  this.catergory.splice(index,1);
+
+   });
+   console.log(' this.catergory', this.catergory)
+  
+    // this.userTypes.pop(item.roleId);
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
+  
+  }
+  onItemSelectOrAll(item:any){
+    this.catergory=this.catArray;
+    console.log("Category Array",this.catergory)
+    this.itemId=item.catId;
+    this.catagoryName=item.catName;
+   this.materialList.onclickcat(this.catergory).subscribe((res)=>{
+     let subcaty = res.response;
+   console.log("responseeee",subcaty);
+this.subCatty = res.response.allOtherSubCAts;
+console.log("SubCategory",this.subCatty);
+this.topping1 = new FormControl(this.subCatty);
+   });
+    console.log("catArray",this.catergory)
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
+}
+  onItemDeSelectOrAll(item:any){
+    this.catergory=[];
+    this.sub_category=[];
+    this.type = [];
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
   }
   onSubCategorySelect(item: any) {
-    console.log(item);
+    console.log(" item Types",item);
+    this.sub_category.push(item.subCatId);
+    this.itemId1=item.subCatId;
+    this.types=item.subCatName;
+   this.materialList.onclicksubcat(this.itemId1).subscribe((res)=>{
+     let typs = res.response;
+   console.log("types..res", typs);
+this.typss =  typs;
+console.log("Typess",this.typss);
+this.topping2 = new FormControl(this.typss);
+   });
+   const data={
+    Cat:this.catergory,
+    Sub_Cat:this.sub_category,
+    type:this.typeI,
+    product:this.productID,
+    status:this.statusTypes,
+    Search:this.searchText
+  }
+  this.materialList.getMaterialList(data).subscribe((res) => {     
+    this.rowData5 = res.response;
+  });
+  }
+  onSubCategoryDeSelect(item: any) {
+
+    this.sub_category.forEach((element,index)=>{
+      if(element==item.subCatId)  this.sub_category.splice(index,1);
+
+   });
+   console.log(' this.sub_category', this.sub_category)
+  
+    // this.userTypes.pop(item.roleId);
+    this.type=[];
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
+  
+  }
+  onSubCategorySelectOrAll(item: any) {
+    console.log(" item Types",item);
+    this.sub_category.push(item.subCatId);
+    this.itemId1=item.subCatId;
+    this.types=item.subCatName;
+   this.materialList.onclicksubcat(this.itemId1).subscribe((res)=>{
+     let typs = res.response;
+   console.log("types..res", typs);
+this.typss =  typs;
+console.log("Typess",this.typss);
+this.topping2 = new FormControl(this.typss);
+   });
+   const data={
+    Cat:this.catergory,
+    Sub_Cat:this.sub_category,
+    type:this.typeI,
+    product:this.productID,
+    status:this.statusTypes,
+    Search:this.searchText
+  }
+  this.materialList.getMaterialList(data).subscribe((res) => {     
+    this.rowData5 = res.response;
+  });
   }
   onTypeSelect(item: any) {
+    this.typeI.push(item.typeId);
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
     console.log(item);
+  }
+  onTypeDeSelect(item: any) {
+
+    this.typeI.forEach((element,index)=>{
+      if(element==item.typeId)  this.catergory.splice(index,1);
+
+   });
+   console.log(' this.catergory', this.catergory)
+  
+    // this.userTypes.pop(item.roleId);
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
+  
   }
   onProductSelect(item: any) {
+    this.productID.push(item.productGroupId);
     console.log(item);
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
   }
+  onProductDeSelect(item: any) {
+    this.productID.forEach((element,index)=>{
+      if(element==item.productGroupId)  this.productID.splice(index,1);
+
+   });
+   console.log(' this.catergory', this.catergory)
+  
+    // this.userTypes.pop(item.roleId);
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
+  
+  }
+  onProductSelectOrAll(item:any){
+    this.productID=this.prodArray;
+    console.log("ProdData",this.ProdData);
+    // this.prodItemId=item.catId;
+    // this.catagoryName=item.catName;
+//    this.materialList.onclickcat(this.ProdData).subscribe((res)=>{
+//      let prod = res.response;
+//    console.log("responseeee",prod);
+// this.prodItem = prod;
+// console.log("Product",this.prodItem);
+// this.topping1 = new FormControl(this.prodItem);
+//    });
+    const data={
+      Cat:this.catergory,
+      Sub_Cat:this.sub_category,
+      type:this.typeI,
+      product:this.productID,
+      status:this.statusTypes,
+      Search:this.searchText
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
+}
+onProductDeSelectOrAll(item: any) {
+  this.productID =[];
+  this.productID.forEach((element,index)=>{
+    if(element==item.productGroupId)  this.productID.splice(index,1);
+
+ });
+ console.log(' this.productID', this.productID)
+
+  // this.userTypes.pop(item.roleId);
+  const data={
+    Cat:this.catergory,
+    Sub_Cat:this.sub_category,
+    type:this.typeI,
+    product:this.productID,
+    status:this.statusTypes,
+    Search:this.searchText
+  }
+  this.materialList.getMaterialList(data).subscribe((res) => {     
+    this.rowData5 = res.response;
+  });
+
+}
   onStatusSelect(item: any) {
+    this.statusTypes.push(item.statusId);
     console.log(item);
+    const data={
+      Cat:[],
+      Sub_Cat:[],
+      type:[],
+      product:[],
+      status:[],
+      Search:""
+    }
+    this.materialList.getMaterialList(data).subscribe((res) => {     
+      this.rowData5 = res.response;
+    });
   }
   editfn(){
     alert('guru')
@@ -279,39 +671,44 @@ this.subCategory = this.fb.group({
     this.product = this.fb.group({
       product: [this.selectedItems]
       });
+      this.catergory =[];
+      this.sub_category =[];
+      this.typeI =[];
+      this.productID =[];
+      this.statusTypes =[];
+      const data={
+        Cat:this.catergory,
+        Sub_Cat:this.sub_category,
+        type:this.typeI,
+        product:this.productID,
+        status:this.statusTypes,
+        Search:this.searchText
+      }
+      this.materialList.getMaterialList(data).subscribe((res) => {     
+        this.rowData5 = res.response;
+      });
   }
   columnDefs: ColDef[] = [
-  { headerName: "User Id",
-    field: 'employeeCode' , sort: 'desc'},
+  { headerName: "Name",
+    field: 'stockItemName',type: ['nonEditableColumn'] , sort: 'desc'},
 
-  {   headerName: "User Name",field: 'employeeName' },
+  {  headerName: "Classification",
+     field: 'classification' },
 
-  { field: 'role', },
-
-  {  headerName: "Email Id",
-     field: 'emailId' },
-
-  {   headerName: "Phone no",
-    field: 'mobilePhone',},
-
-  {   headerName: "Last Login",
-    // field: 'lastLoginDate',type: ['dateColumn', 'nonEditableColumn'], width: 220  },
-    field: 'lastLoginDate',type: ['nonEditableColumn']},
-
-
+  {   headerName: "UoM",
+    field: 'uoMName',type: ['nonEditableColumn']},
+    {   headerName: "Product Group",
+    field: 'productGroupName',type: ['nonEditableColumn']},
+    {   headerName: "SKU",
+    field: 'productSKUName',type: ['nonEditableColumn']},
   { headerName: "Status",
-     field: 'status', 
+
+     field: 'statusName', 
   cellEditor: 'agSelectCellEditor',
   cellEditorParams: {
     values: ['Active', 'Inactive', 'Invited', 'Locked',],
   }
 },
-// {
-//   headerName: "Avatar",
-//   field: "avatar",
-//   width: 100,
-//   cellRenderer: `<img style="height: 14px; width: 14px" src='../../../assets/img/edit.svg' />`
-//  },
 
 ];
 public defaultColDef: ColDef = {
---
+++ angular-ui/src/app/services/material-list.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpEvent, HttpHandler, HttpHeaders, HttpParams, HttpRequest, JsonpClientBackend } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class MaterialListService {
+  classificationurl = environment.url;
+  constructor(private http: HttpClient) { }
+
+  intercept(req: HttpRequest<any>,
+    next: HttpHandler): Observable<HttpEvent<any>> {
+
+const idToken = localStorage.getItem("token");
+debugger
+console.log('idtoken',idToken)
+alert(idToken)
+
+if (idToken) {
+  const cloned = req.clone({
+      headers: req.headers.set("Authorization",
+          "Bearer " + idToken)
+  });
+
+  return next.handle(cloned);
+}
+else {
+  return next.handle(req);
+}
+    }
+  getclassification(){
+    return this.http.get<any>(this.classificationurl + 'MaterialApi/GetCategories');
+  
+  }
+  onclickcat(catId:any){
+    return this.http.get<any>(`${this.classificationurl}MaterialApi/GetSUbCAts?catid=${catId}`);
+  
+  }
+  
+  onclicksubcat(catId:any){
+    return this.http.get<any>(`${this.classificationurl}MaterialApi/GetTypes?subCatId=${catId}`);
+  
+  }
+  getProduct(){
+    return this.http.get<any>(this.classificationurl + 'MaterialApi/GetProductGroupList');
+  
+  }
+  getStatusList(){
+    return this.http.get<any>(this.classificationurl + 'MaterialApi/GetStatusList');
+  
+  }
+  public getMaterialList(data) : Observable<any>{
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    };
+    JSON.stringify(data)
+        return this.http.post<any>(this.classificationurl+'MaterialApi/GetMaterailList',data);
+
+      }
+}

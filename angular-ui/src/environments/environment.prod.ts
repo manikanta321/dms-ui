5cc2c7e2a2ac8419a567d0f1388d6af23cbab2a7
Manikanta_dk <Manikanta.dk@smartgig.tech>
2022-11-07 12:12:02 +0530

    prod url

--- angular-ui/src/environments/environment.prod.ts
+++ angular-ui/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://52.172.24.161:801/api/',
+  // url: 'http://52.172.24.161:801/api/',
+  url: 'http://13.126.235.145:801/api/',
+
 };

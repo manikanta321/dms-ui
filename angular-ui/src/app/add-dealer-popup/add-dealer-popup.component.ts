6ff8525ae4aa6904c0dd0ef5d9da3e70de466f5c
aakash_pal <c_aakash.pal@infinitylearn.com>
2022-11-26 22:18:18 +0530

    Dealer geography issue fix

--- angular-ui/src/app/add-dealer-popup/add-dealer-popup.component.ts
+++ angular-ui/src/app/add-dealer-popup/add-dealer-popup.component.ts
@@ -1,10 +1,10 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators  } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatStepper } from '@angular/material/stepper';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 //import { AddItemsPromotionComponent } from '/add-items-promotion/add-items-promotion.component';
-import { FormArray } from '@angular/forms' 
+import { FormArray } from '@angular/forms'
 import { UserService } from 'src/app/services/user.service';
 import { ClassificationserviseService } from 'src/app/services/classificationservise.service';
 import { HostListener } from '@angular/core';
@@ -23,56 +23,56 @@ export class AddDealerPopupComponent implements OnInit {
   addAddressDetailsForm!: FormGroup;
   gepGraphiesFormGroup!: FormGroup;
 
-  LoginId:any;
-  numberValue:any;
-  statusList:any;
-  addAddress:boolean = false;
+  LoginId: any;
+  numberValue: any;
+  statusList: any;
+  addAddress: boolean = false;
   selectedTeam = '';
   showDiv = {
-    previous : false,
-    current : false,
-    next : false
+    previous: false,
+    current: false,
+    next: false
   }
-  payloadArray={
-    enabled:false
+  payloadArray = {
+    enabled: false
   }
   errorMsg: any;
   addCountryButton: boolean = false;
 
-  basicInfo:boolean=false;
+  basicInfo: boolean = false;
 
   removelist: boolean = false;
   stateName: string[] = ['State 1', 'State 2',];
 
 
 
-  CountryList:any=[];
-  stateList: any=[];
-  distList:any=[];
-  cityList:any=[];
+  CountryList: any = [];
+  stateList: any = [];
+  distList: any = [];
+  cityList: any = [];
 
-  catagoryroouting='';
-  subcatRoouting='';
-  selectedtypeItem='';
-  subcatcount='';
-  typecount='';
-  ConsigneeName1:any;
-  ConsigneeName2:any;
-  stateselectedItem:any;
-  distselectedItem:any;
-  citySelectedItem:any;
+  catagoryroouting = '';
+  subcatRoouting = '';
+  selectedtypeItem = '';
+  subcatcount = '';
+  typecount = '';
+  ConsigneeName1: any;
+  ConsigneeName2: any;
+  stateselectedItem: any;
+  distselectedItem: any;
+  citySelectedItem: any;
 
   selectedItem = null;
   totalStepsCount: number | undefined;
 
-  dealerAction:any;
-  addType:any;
+  dealerAction: any;
+  addType: any;
   geoGraphyHirerachyData: any;
   geoGraphyFullData: any;
-  geoGraphyFullData1: any;
-  geographyHierarchyId:any;
-  aarrayToPush:any[]=[];
-  css:any[]=[];
+  // geoGraphyFullData1: any;
+  geographyHierarchyId: any;
+  aarrayToPush: any[] = [];
+  css: any[] = [];
 
   colorsList = [
     { primaryColor: { background: '#00187A', color: '#fff' }, secondaryColor: { background: "#EAEEFF", color: "#00187A" }, },
@@ -89,42 +89,42 @@ export class AddDealerPopupComponent implements OnInit {
     { primaryColor: { background: '#DC0063', color: '#fff' }, secondaryColor: { background: "#FFE1EE", color: "#DC0063" }, },
   ];
 
- 
+
 
   @ViewChild('stepper') private myStepper: MatStepper | any;
-  
+
   // CategoryName:any;
-  getgroup : string[]= ["Product Name","Product Name", "Product Name", "Product Name"]
-  buygroup : string[]= ["Product Name","Product Name", "Product Name", "Product Name"];
-  CustomerSelect : string[] = ['Valiant Distributors', 'Global Movers', 'Somebody Sales']
- 
-  constructor(private _formBuilder: FormBuilder, public dialog: MatDialog,private spinner: NgxSpinnerService,
+  getgroup: string[] = ["Product Name", "Product Name", "Product Name", "Product Name"]
+  buygroup: string[] = ["Product Name", "Product Name", "Product Name", "Product Name"];
+  CustomerSelect: string[] = ['Valiant Distributors', 'Global Movers', 'Somebody Sales']
+
+  constructor(private _formBuilder: FormBuilder, public dialog: MatDialog, private spinner: NgxSpinnerService,
     private user: UserService,
     private classification: ClassificationserviseService,
-    private calssification:ClassificationserviseService,
+    private calssification: ClassificationserviseService,
     //private toastrService: ToastrService,
     private dialogRef: MatDialogRef<any>) {
-      this.formReader();
-     }
+    this.formReader();
+  }
 
-     
-  firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
-  secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
+
+  firstFormGroup: FormGroup = this._formBuilder.group({ firstCtrl: [''] });
+  secondFormGroup: FormGroup = this._formBuilder.group({ secondCtrl: [''] });
 
   ngOnInit(): void {
-    this.LoginId=localStorage.getItem("logInId");
+    this.LoginId = localStorage.getItem("logInId");
     this.numberValue = Number(this.LoginId);
-     this.addAddressForm1();
-     this.addAddressForm1();
+    this.addAddressForm1('1');
+    this.addAddressForm1('2');
 
-     this.statusForm();
-     //geographies List
-     this.getGeographyHierarchy();
-     if(localStorage.getItem('edit-dealer') === 'Edit'){
+    this.statusForm();
+    //geographies List
+    this.getGeographyHierarchy();
+    if (localStorage.getItem('edit-dealer') === 'Edit') {
       this.dealerAction = "Edit"
-     } else {
+    } else {
       this.dealerAction = "Add"
-     }
+    }
     //  this.ConsigneeName2='';
   }
 
@@ -139,23 +139,23 @@ export class AddDealerPopupComponent implements OnInit {
       this.spinner.hide();
       this.geoGraphyHirerachyData = res.response;
 
-      console.log('geoGraphyHirerachyData',this.geoGraphyHirerachyData);
+      console.log('geoGraphyHirerachyData', this.geoGraphyHirerachyData);
 
 
       this.geoGraphyFullData = JSON.parse(JSON.stringify(res.response));
-      console.log('geoGraphyFullData',this.geoGraphyFullData);
-      const hdata=this.geoGraphyFullData.find(a=>a.primaryGeographyAssociation=='Y');
+      console.log('geoGraphyFullData', this.geoGraphyFullData);
+      const hdata = this.geoGraphyFullData.find(a => a.primaryGeographyAssociation == 'Y');
 
-      console.log('hdata',hdata)
+      console.log('hdata', hdata)
 
-      this.geographyHierarchyId=hdata.geographyHierarchyId
-      const data=this.geoGraphyFullData.findIndex(a=>a.primaryGeographyAssociation=='Y');
-     this.geoGraphyFullData1=this.geoGraphyFullData.slice(0,data+1)
+      this.geographyHierarchyId = hdata.geographyHierarchyId
+      const data = this.geoGraphyFullData.findIndex(a => a.primaryGeographyAssociation == 'Y');
+      this.geoGraphyFullData = this.geoGraphyFullData.slice(0, data + 1)
 
-      console.log('data to console1',this.geoGraphyFullData.slice(0,data+1))
-    
-      
-      console.log('geoGraphyFullData',this.geoGraphyFullData);
+      console.log('data to console1', this.geoGraphyFullData.slice(0, data + 1))
+
+
+      console.log('geoGraphyFullData', this.geoGraphyFullData);
 
       this.getGeographiesDataById(null, 1);
     }, err => {
@@ -171,24 +171,24 @@ export class AddDealerPopupComponent implements OnInit {
     console.log(clickedItem, hirerachyIndex);
 
 
-if(this.geographyHierarchyId==hirerachyIndex){
-this.css.push(clickedItem)
+    // if (this.geographyHierarchyId == hirerachyIndex) {
+    //   this.css.push(clickedItem)
 
-  const index = this.aarrayToPush.indexOf(clickedItem.geographyId);
+    //   const index = this.aarrayToPush.indexOf(clickedItem.geographyId);
 
-  if (index !== -1) {
-    this.aarrayToPush.splice(index, 1);
-  }
-  else{
-    this.aarrayToPush.push(clickedItem.geographyId);
+    //   if (index !== -1) {
+    //     this.aarrayToPush.splice(index, 1);
+    //   }
+    //   else {
+    //     this.aarrayToPush.push(clickedItem.geographyId);
 
-  }
- 
-console.log('aarrayToPush',this.aarrayToPush)
-}
-else{
-  this.aarrayToPush=[]
-}
+    //   }
+
+    //   console.log('aarrayToPush', this.aarrayToPush)
+    // }
+    // else {
+    //   this.aarrayToPush = []
+    // }
 
     this.geoGraphyFullData[hirerachyIndex - 1].allOtherGeography.forEach(element => {
       if (element.geographyId == clickedItem.geographyId) {
@@ -212,6 +212,7 @@ else{
 
 
     });
+    console.log(this.geoGraphyFullData);
   }
 
 
@@ -245,19 +246,19 @@ else{
 
 
   //status dropdownlist
-  onKeyName(event){
-     this.ConsigneeName1 =event.target.value;
+  onKeyName(event) {
+    this.ConsigneeName1 = event.target.value;
   }
-  Checked(){
+  Checked() {
     this.ConsigneeName2 = this.ConsigneeName1;
   }
-  statusForm(){
+  statusForm() {
     this.user.getstatusDeatils().subscribe((res: any) => {
-        this.statusList = res.response;
+      this.statusList = res.response;
     })
   }
 
-  
+
   ngAfterViewInit() {
     this.totalStepsCount = this.myStepper._steps.length;
   }
@@ -266,179 +267,187 @@ else{
     stepper.next();
   }
 
-  disableBackbutton(){
-       this.goForward(this.myStepper);
-      this.basicInfo=true;
-      // alert(this.basicInfo);
-    } 
-   
-  
-  
-
-
-  getCategory(event: any){
-    if (event.CategoryName=='Buy(A+B..) get(X+Y..)'){
-      this.goForward(this.myStepper);
-      }
-      if (event.CategoryName=='Price Discount'){
-        this.goForward(this.myStepper);
-        }
-        if (event.CategoryName=='Volume Discount'){
-          this.goForward(this.myStepper);
-          }
-    }
-
-
-  quantities(): FormArray {  
-    return this.addAddressDetailsForm.controls["quantities"] as FormArray  
-  }  
-     
-  initAddress(): FormGroup {  
-    return this._formBuilder.group({  
-      addType:['',[Validators.required]], 
-      consigName:['',[Validators.required]],
-      tax: ['',[Validators.required]],
-      addressLine: ['',[Validators.required]],
-      addressLine1: ['',[Validators.required]],
-      country: ['',[Validators.required]],
-      state: ['',[Validators.required]],
-      cityAndZip: ['',[Validators.required]],
-      Zipcode: ['',[Validators.required]],
-      phoneNo: ['',[Validators.required]],  
-    })  
-  }  
-     
-  addAddressForm() {  
-    this.addAddress =true;
-    this.quantities().push(this.initAddress());  
+  disableBackbutton() {
+    this.goForward(this.myStepper);
+    this.basicInfo = true;
+    // alert(this.basicInfo);
   }
-  
-  addAddressForm1() {  
-    this.addType=1;
-
-    this.addAddress =true;
-    this.quantities().push(this.initAddress());  
-  } 
 
 
 
-  
-  removeQuantity(i:number) { 
-    if( i >= 2){
-      this.quantities().removeAt(i);  
+
+
+  getCategory(event: any) {
+    if (event.CategoryName == 'Buy(A+B..) get(X+Y..)') {
+      this.goForward(this.myStepper);
     }
-  }  
+    if (event.CategoryName == 'Price Discount') {
+      this.goForward(this.myStepper);
+    }
+    if (event.CategoryName == 'Volume Discount') {
+      this.goForward(this.myStepper);
+    }
+  }
 
 
-  saveDealerData(){
+  quantities(): FormArray {
+    return this.addAddressDetailsForm.controls["quantities"] as FormArray
+  }
+
+  initAddress(defaultType): FormGroup {
+    defaultType = defaultType ?? '';
+    console.log(defaultType);
+    return this._formBuilder.group({
+      addType: [defaultType, [Validators.required]],
+      consigName: ['', [Validators.required]],
+      tax: ['', [Validators.required]],
+      addressLine: ['', [Validators.required]],
+      addressLine1: ['', [Validators.required]],
+      country: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      cityAndZip: ['', [Validators.required]],
+      Zipcode: ['', [Validators.required]],
+      phoneNo: ['', [Validators.required]],
+    })
+  }
+
+  addAddressForm() {
+    this.addAddress = true;
+    this.quantities().push(this.initAddress(''));
+  }
+
+  addAddressForm1(defaultType) {
+    // this.addType = 1;
+
+    this.addAddress = true;
+    this.quantities().push(this.initAddress(defaultType));
+  }
+
+
+
+
+  removeQuantity(i: number) {
+    if (i >= 2) {
+      this.quantities().removeAt(i);
+    }
+  }
+
+
+  saveDealerData() {
     this.goForward(this.myStepper);
     console.log(this.addAddressDetailsForm.value);
   }
 
- 
-  formReader(){
-    this.addAddressDetailsForm = this._formBuilder.group({  
+
+  formReader() {
+    this.addAddressDetailsForm = this._formBuilder.group({
       dealerName: ['', [Validators.required]],
       email: ['', [Validators.required]],
-      dealerCode:['', [Validators.required]],
+      dealerCode: ['', [Validators.required]],
       website: ['', [Validators.required]],
       phone: ['', [Validators.required]],
       companyId: ['', [Validators.required]],
       identifier: ['', [Validators.required]],
       userName: ['', [Validators.required]],
-      userEmail:['', [Validators.required]],
+      userEmail: ['', [Validators.required]],
       usermobile: ['', [Validators.required]],
-      firstName:['', [Validators.required]],
-      lastName:['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
       status: ['', [Validators.required]],
-      quantities: this._formBuilder.array([]) ,  
-    });  
+      quantities: this._formBuilder.array([]),
+    });
   }
 
   //get Country List  - geographies
-  getCountryList(){
-    
-    this.calssification.getCountryList().subscribe((res)=>{
-          let data=res.response;
-          this.CountryList=data.allOtherCountries;
-          this.getStateList(data.firstCountr.countryId);
-          this.selectedItem=data.firstCountr.countryId;
-           
-        })  
+  getCountryList() {
+
+    this.calssification.getCountryList().subscribe((res) => {
+      let data = res.response;
+      this.CountryList = data.allOtherCountries;
+      this.getStateList(data.firstCountr.countryId);
+      this.selectedItem = data.firstCountr.countryId;
+
+    })
   }
 
   //get State List
-  getStateList(id:any){
+  getStateList(id: any) {
     this.stateList.length = 0;
     this.distList.length = 0;
     this.cityList.length = 0;
 
-    localStorage.setItem('cityId','');
-    localStorage.setItem("stateId",'');
-    localStorage.setItem('distId','');
+    localStorage.setItem('cityId', '');
+    localStorage.setItem("stateId", '');
+    localStorage.setItem('distId', '');
 
-    localStorage.setItem('countryId',id);
-    this.selectedItem= id;
-    this.calssification.getAllListByCountry(id).subscribe((res)=>{
-          let data=res.response;
-          this.stateList=data.allOtherGeography;
-          this.stateselectedItem = data.firstGeography.geographyId;
-           this.getDistrictList(data.firstGeography.geographyId)
-    
+    localStorage.setItem('countryId', id);
+    this.selectedItem = id;
+    this.calssification.getAllListByCountry(id).subscribe((res) => {
+      let data = res.response;
+      this.stateList = data.allOtherGeography;
+      this.stateselectedItem = data.firstGeography.geographyId;
+      this.getDistrictList(data.firstGeography.geographyId)
 
-        })
+
+    })
   }
 
   //get Dist List
-  getDistrictList(id:any){
+  getDistrictList(id: any) {
     this.cityList.length = 0;
-    localStorage.setItem('cityId','');
-    localStorage.setItem('distId','');
+    localStorage.setItem('cityId', '');
+    localStorage.setItem('distId', '');
     this.stateselectedItem = id;
-    localStorage.setItem("stateId",id);
-    this.calssification.getAllListByCountry(id).subscribe((res)=>{
-          let data=res.response;
-          this.distList=data.allOtherGeography;
-          this.distselectedItem = data.firstGeography.geographyId;
-          this.getCityList(data.firstGeography.geographyId);
-        })
+    localStorage.setItem("stateId", id);
+    this.calssification.getAllListByCountry(id).subscribe((res) => {
+      let data = res.response;
+      this.distList = data.allOtherGeography;
+      this.distselectedItem = data.firstGeography.geographyId;
+      this.getCityList(data.firstGeography.geographyId);
+    })
   }
 
   //get Dist List
-  getCityList(id:any){
-   
+  getCityList(id: any) {
+
     this.distselectedItem = id;
-    localStorage.setItem('distId',id);
-    this.calssification.getAllListByCountry(id).subscribe((res)=>{
-          let data=res.response;
-          this.cityList=data.allOtherGeography;
-          this.citySelectedItem = data.firstGeography.geographyId;
-          this.getSelectedCity(data.firstGeography.geographyId);
-        })
+    localStorage.setItem('distId', id);
+    this.calssification.getAllListByCountry(id).subscribe((res) => {
+      let data = res.response;
+      this.cityList = data.allOtherGeography;
+      this.citySelectedItem = data.firstGeography.geographyId;
+      this.getSelectedCity(data.firstGeography.geographyId);
+    })
   }
 
-  getSelectedCity(id:any){
-    localStorage.setItem('cityId',id);
+  getSelectedCity(id: any) {
+    localStorage.setItem('cityId', id);
     this.citySelectedItem = id;
   }
 
-  saveGeographiesList(){
+  saveGeographiesList() {
 
+    let selectedGeographies = this.geoGraphyFullData[this.geoGraphyFullData.length - 1].geographySelected;
+    if (selectedGeographies.length == 0) {
+      alert("Please select default geography grid");
+      return;
+    }
+    console.log(selectedGeographies);
     let countrydata = {
-      "CountryId":localStorage.getItem('countryId'),
-      "StateId":localStorage.getItem('stateId'),
-      "RegionId":localStorage.getItem('distId'),
-      "CityId":localStorage.getItem('cityId'),
-      "CreatedById":this.LoginId
-      };
+      "CountryId": localStorage.getItem('countryId'),
+      "StateId": localStorage.getItem('stateId'),
+      "RegionId": localStorage.getItem('distId'),
+      "CityId": localStorage.getItem('cityId'),
+      "CreatedById": this.LoginId
+    };
 
-      let data = Object.assign(this.addAddressDetailsForm.value, countrydata)
+    let data = Object.assign(this.addAddressDetailsForm.value, countrydata)
 
-      console.log(data);
+    console.log(data);
 
-     this.calssification.addDealerData(data).subscribe((res)=>{
-            console.log(res);
-        });  
+    this.calssification.addDealerData(data).subscribe((res) => {
+      console.log(res);
+    });
 
   }
 

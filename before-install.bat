3f35d123d4a32f812344c53edab480ad822e199b
manikanta321 <52309611+manikanta321@users.noreply.github.com>
2022-12-05 15:40:09 +0530

    Create before-install.bat

---
+++ before-install.bat
@@ -0,0 +1,2 @@
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server

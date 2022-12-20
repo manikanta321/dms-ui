afbf10e55853229f0598f5eff3280a739c71cc90
Teja <krishna.teja@smartgig.tech>
2022-12-21 00:06:08 +0530

    Add Order non promotion items

--- angular-ui/src/app/component/orders/addorderpromotions/addorderpromotions.component.ts
+++ angular-ui/src/app/component/orders/addorderpromotions/addorderpromotions.component.ts
@@ -16,11 +16,11 @@ import { OrdersApisService } from 'src/app/services/orders-apis.service';
   styleUrls: ['./addorderpromotions.component.css']
 })
 export class AddorderpromotionsComponent implements OnInit {
-  
+
   selectedTeam = '';
   selectedDay: string = '';
 
-  dealerInfo = false;  
+  dealerInfo = false;
   orderitem = false;
   otherInfo = false;
   image1 = 'assets/img/minimize-tag.png';
@@ -31,95 +31,95 @@ export class AddorderpromotionsComponent implements OnInit {
   updateOrSave: boolean = false
   editData: boolean = false;
   // non_promotins
-  orderNonPromotionsdata:any = [];
-  taxdropdowndata:any = [];
+  orderNonPromotionsdata: any = [];
+  taxdropdowndata: any = [];
   // buyGroup : any = [{proItem: 'Lays IPL edition classic magic masala..', sku:'KA123458AB98764',price:'20' , taxtemplete:['hj','hj'], amount:'0'},
   // {proItem: 'Lays IPL edition classic magic masala..', sku:'KA123458AB98764',price:'20' , taxtemplete:['hj','hj'], amount:'0'},
   // {proItem: 'Lays IPL edition classic magic masala..', sku:'KA123458AB98764',price:'20' , taxtemplete:['hj','hj'], amount:'0'}]
-  
+
   dropdownSettingscat: IDropdownSettings = {};
   dropdownSettingssubcat: IDropdownSettings = {};
   dropdownSettingstypeid: IDropdownSettings = {};
   dropdownSettingsmaterialid: IDropdownSettings = {};
-  countCatagory:any;
-  catagoryData:any;
+  countCatagory: any;
+  catagoryData: any;
   toppings: any = [];
-  categoryMapData:any =[];
-  categoryArray:any= [];
+  categoryMapData: any = [];
+  categoryArray: any = [];
   catergory: any = [];
-  flag:boolean=true;
+  flag: boolean = true;
   ShowFilter = false;
-  Non_promotions :boolean = false;
+  Non_promotions: boolean = false;
   private gridApi!: GridApi;
   promoList = true;
-  myForms!:FormGroup;
+  myForms!: FormGroup;
   categoryForm: any = FormGroup;
   subcategoryForm: any = FormGroup;
   subCategoryFilter = false;
   typeFilter = false;
-  type : any =[];
-  myFormsIdentifier:any = FormGroup;
+  type: any = [];
+  myFormsIdentifier: any = FormGroup;
   selectedItems: any = [];
   sub_category: any = [];
-  sub_categorys : any= [];
-  typesData : any= [];
+  sub_categorys: any = [];
+  typesData: any = [];
   materialIdentifierData: any = [];
   topping1: any = [];
   topping2: any = [];
-  subcatagData:any=[];
-  subcatArray:any=[];  
+  subcatagData: any = [];
+  subcatArray: any = [];
   disabled = false;
-  typeI:any =[];
-  
-  typesMapData:any=[];
-  typesArray:any=[];
-  materialIdentifier:any = [];
-  materialIdentifierMapData:any=[];
-  materialIdentifierArray:any=[];
+  typeI: any = [];
+
+  typesMapData: any = [];
+  typesArray: any = [];
+  materialIdentifier: any = [];
+  materialIdentifierMapData: any = [];
+  materialIdentifierArray: any = [];
 
   //event handler for the select element's change event
-  selectChangeHandler (event: any) {
+  selectChangeHandler(event: any) {
     //update the ui
     this.selectedDay = event.target.value;
   }
-/*-------*/
-  countryname: string[] = ['Malaysia (71/126)', 'India (178/178)','Philipines (0/135)'];
-  statename: string[] = ['Johor(0/42)', 'Kedah(36/36','Perak(14/26)','Penang(21/22)'];
-  regionname: string[] = ['North(4/4)', 'South(8/8)', 'East(6/6)','West(3/4)'];
-  cityname:string[] =['George town','Balik Pulau','Batu Refringi','Teluk Bahang'];
+  /*-------*/
+  countryname: string[] = ['Malaysia (71/126)', 'India (178/178)', 'Philipines (0/135)'];
+  statename: string[] = ['Johor(0/42)', 'Kedah(36/36', 'Perak(14/26)', 'Penang(21/22)'];
+  regionname: string[] = ['North(4/4)', 'South(8/8)', 'East(6/6)', 'West(3/4)'];
+  cityname: string[] = ['George town', 'Balik Pulau', 'Batu Refringi', 'Teluk Bahang'];
   selectedItem = null;
-  addButton:boolean =false;
+  addButton: boolean = false;
   dropdownSettings1: IDropdownSettings = {};
   dropdownSettings2: IDropdownSettings = {};
   dropdownSettings3: IDropdownSettings = {};
-  selectgeo:  any= ['country','state'];
-  selectbillAddress :any = ['address1','address2'];
-  selectShippingAddress :any =['shippingAddress1', 'shipping2']
-  getgroup : string[]= ["Product Name","Product Name", "Product Name", "Product Name"]
-  buygroup : string[]= ["Product Name","Product Name", "Product Name", "Product Name"];
-  CustomerSelect : string[] = ['Valiant Distributors', 'Global Movers', 'Somebody Sales']
+  selectgeo: any = ['country', 'state'];
+  selectbillAddress: any = ['address1', 'address2'];
+  selectShippingAddress: any = ['shippingAddress1', 'shipping2']
+  getgroup: string[] = ["Product Name", "Product Name", "Product Name", "Product Name"]
+  buygroup: string[] = ["Product Name", "Product Name", "Product Name", "Product Name"];
+  CustomerSelect: string[] = ['Valiant Distributors', 'Global Movers', 'Somebody Sales']
   public itemremoved: any[] = [{
     sValue: '',
     eValue: '',
     pValue: '',
   }];
-  constructor(private _formBuilder: FormBuilder,private spinner: NgxSpinnerService,
+  constructor(private _formBuilder: FormBuilder, private spinner: NgxSpinnerService,
     private http: HttpClient,
-    private orders:OrdersApisService,
+    private orders: OrdersApisService,
     private materialList: MaterialListService,
     private addMaterials: AddMaterialsService,
-    private fb: FormBuilder,private dialog: MatDialog,
+    private fb: FormBuilder, private dialog: MatDialog,
     private dialogRef: MatDialogRef<any>) { }
 
-  firstFormGroup: FormGroup = this._formBuilder.group({firstCtrl: ['']});
-  secondFormGroup: FormGroup = this._formBuilder.group({secondCtrl: ['']});
+  firstFormGroup: FormGroup = this._formBuilder.group({ firstCtrl: [''] });
+  secondFormGroup: FormGroup = this._formBuilder.group({ secondCtrl: [''] });
 
   ngOnInit(): void {
     this.orderNonPromotionsList();
     this.taxdropdown();
     this.getclassification();
     this.selectMaterialIdentifier();
-     this.dropdownSettingssubcat = {
+    this.dropdownSettingssubcat = {
       singleSelection: false,
       idField: 'subCatId',
       textField: 'subCatName',
@@ -159,103 +159,108 @@ export class AddorderpromotionsComponent implements OnInit {
       identifiers: [this.selectedItems]
     });
     let editV = localStorage.getItem('Edit');
-    
+
     if (editV == 'Edit') {
       this.actineLabel = "Edit order";
       this.updateOrSave = !this.updateOrSave
       // this.spinner.show();
       // this.spinner.hide();
 
-}
-else {
+    }
+    else {
       this.actineLabel = "Add order";
       this.editData = false;
       // this.updateOrSave= this.updateOrSave;
-      
+
     }
 
 
   }
 
-  
+
+  checkboxChange(event, changedPromotionObj){
+    console.log(event, changedPromotionObj);
+    changedPromotionObj.isPromotionSelected = event.target.checked;
+  }
+
   onTypeAll(items: any) {
     console.log('onSelectAll', items);
   }
   onClick(item) {
     this.selectedItem = item;
   }
-   addCategory(){
-    this.addButton =true;
+  addCategory() {
+    this.addButton = true;
   }
 
-  expandDealerInfoDiv(){
+  expandDealerInfoDiv() {
     this.dealerInfo = !this.dealerInfo;
 
-    if(this.dealerInfo === false){
+    if (this.dealerInfo === false) {
       this.image1 = 'assets/img/maximize-arrow.png';
     } else {
       this.image1 = 'assets/img/minimize-tag.png';
-     
+
     }
   }
 
-  expandOrderItemsDiv(){
+  expandOrderItemsDiv() {
     this.orderitem = !this.orderitem;
 
-    if(this.orderitem === false){
+    if (this.orderitem === false) {
       this.image2 = 'assets/img/minimize-tag.png';
     } else {
       this.image2 = 'assets/img/maximize-arrow.png';
     }
-    
+
   }
 
-  expandOtherInfoDiv(){
+  expandOtherInfoDiv() {
     this.otherInfo = !this.otherInfo;
 
-    if(this.otherInfo === false){
+    if (this.otherInfo === false) {
       this.image3 = 'assets/img/minimize-tag.png';
     } else {
       this.image3 = 'assets/img/maximize-arrow.png';
     }
   }
 
-  addOrderPromotionList(){
-    this.dialog.open( AddOrderPromotionlistComponent,{width: '1043px',height:'900px'});
-    localStorage.setItem('buygroupromo','')
+  addOrderPromotionList() {
+    this.dialog.open(AddOrderPromotionlistComponent, { width: '1043px', height: '900px' });
+    localStorage.setItem('buygroupromo', '')
   }
 
-  addOrderNonPromotionList(){
+  addOrderNonPromotionList() {
     // this.dialog.open( OrderNonpromotionlistComponent,{width: '1043px',height:'663px'});
     this.Non_promotions = true;
   }
   removeItem() {
     this.itemremoved.splice(0);
   }
-// non-prmotions
+  // non-prmotions
 
-refresh() {
-  this.categoryForm = this.fb.group({
-    categoryy: [this.selectedItems]
-  });
-  this.subcategoryForm = this.fb.group({
-    subCategory: [this.selectedItems]
-  });
-  this.type = this.fb.group({
-    type: [this.selectedItems]
-  });
-  this.myFormsIdentifier = this.fb.group({
-    identifiers: [this.selectedItems]
-  });
-  this.catergory = [];
-  this.sub_categorys = [];
-  this.typesData = [];
-  this.materialIdentifierData = [];
-}
+  refresh() {
+    this.categoryForm = this.fb.group({
+      categoryy: [this.selectedItems]
+    });
+    this.subcategoryForm = this.fb.group({
+      subCategory: [this.selectedItems]
+    });
+    this.type = this.fb.group({
+      type: [this.selectedItems]
+    });
+    this.myFormsIdentifier = this.fb.group({
+      identifiers: [this.selectedItems]
+    });
+    this.catergory = [];
+    this.sub_categorys = [];
+    this.typesData = [];
+    this.materialIdentifierData = [];
+  }
   closePopup() {
     this.Non_promotions = false
     // this.dialogRef.close();
-  
+
   }
 
   getclassification() {
@@ -302,7 +307,7 @@ refresh() {
     // this.catagoryName = item.catName;
     let Subdata = {
       catId: this.catergory,
-      flag:this.flag
+      flag: this.flag
     }
     this.materialList.onclickcat(Subdata).subscribe((res) => {
       let subcaty = res.response;
@@ -352,7 +357,7 @@ refresh() {
       this.subcatagData = allSub_cats.map((data: { subCatId: any; subCatName: any; }) => {
         return { subCatId: data.subCatId, subCatName: data.subCatName };
       });
-  
+
       if (!this.subcatagData?.length) {
         this.subcatagData = allSub_cats.map((subCatData: { designationName: any; }) => {
           return subCatData.designationName;
@@ -361,7 +366,7 @@ refresh() {
       this.subcatagData.push()
       this.subcatagData.forEach(element => {
         return this.subcatArray.push(element.subCatId);
-  
+
       })
       this.topping1 = new FormControl(this.sub_category);
     });
@@ -381,12 +386,12 @@ refresh() {
 
   }
   // sub cat
-  onSubCategorySelect(item:any) {
+  onSubCategorySelect(item: any) {
     console.log(" item Types", item);
     this.sub_categorys.push(item.subCatId);
     let Type = {
       subCatId: this.sub_categorys,
-      flag:this.flag
+      flag: this.flag
     }
     this.materialList.onclicksubcat(Type).subscribe((res) => {
       let typs = res.response;
@@ -477,7 +482,7 @@ refresh() {
   }
   onMaterialIdentifierSelect(item: any) {
     this.materialIdentifierData.push(item.materilCustomIdentifierId);
-    console.log("materialIdentifier",this.materialIdentifierData);
+    console.log("materialIdentifier", this.materialIdentifierData);
   }
   onMaterialIdentifierDeSelect(item: any) {
 
@@ -485,7 +490,7 @@ refresh() {
       if (element == item.materilCustomIdentifierId) this.materialIdentifierData.splice(index, 1);
 
     });
-    console.log("materialIdentifier",this.materialIdentifierData);
+    console.log("materialIdentifier", this.materialIdentifierData);
   }
   onMaterialIdentifierSelectOrAll() {
 
@@ -504,16 +509,16 @@ refresh() {
 
     })
     this.materialIdentifierData = this.materialIdentifierArray;
-    console.log("materialIdentifier",this.materialIdentifierData);
+    console.log("materialIdentifier", this.materialIdentifierData);
   }
   onMaterialIdentifierDeSelectOrAll() {
     this.materialIdentifierData = [];
-    console.log("materialIdentifier",this.materialIdentifierData);
+    console.log("materialIdentifier", this.materialIdentifierData);
   }
 
 
   // non promotions list table data
-  orderNonPromotionsList(){
+  orderNonPromotionsList() {
     const data =
     {
       "Cat": [],
@@ -522,17 +527,45 @@ refresh() {
       "MaterialCustomIdentifier": [],
       "Search": ""
     }
-    
+
     this.orders.getorderNonPromotionslist(data).subscribe((res) => {
-      this.orderNonPromotionsdata = res.response;
+      // this.orderNonPromotionsdata = res.response;
+      let orderNonPromotionsData = res.response;
+
+      this.orderNonPromotionsdata = this.orderNonPromotionFormatter(orderNonPromotionsData);
+
     });
   }
+
+  orderNonPromotionFormatter(items){
+    let formattedList:any = [];
+    items.forEach( item =>{
+      let obj:any = {}
+      obj.classification = item.calssification; 
+      obj.materialCustomName = item.materialCustomName;
+      obj.mrp =item.mrp;
+      obj.productSKUName =item.productSKUName;
+      obj.stockItemId =item.stockItemId;
+      obj.stockItemName =item.stockItemName;
+      obj.isPromotionSelected = false;
+      obj.Quantity = null;
+      obj.Taxid = null;
+      formattedList.push(obj);
+    });
+
+    return formattedList;
+
+  }
   
-  taxdropdown(){
+  addnonPromoItems(){
+    console.log(this.orderNonPromotionsdata);
+  }
+
+  taxdropdown() {
     this.orders.taxtemplatedropdown().subscribe((res) => {
       this.taxdropdowndata = res.response;
-      console.log(this.taxdropdowndata,"tax data")
-      
+      console.log(this.taxdropdowndata, "tax data")
+
     });
   }
 }

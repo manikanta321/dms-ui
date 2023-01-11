429b9e5e57547c8cf5b992986c2bef146f38db2f
Teja <krishna.teja@smartgig.tech>
2023-01-11 09:25:16 +0530

    Other masters 404 issue fixed

--- angular-ui/src/app/component/other-masters/other-masters.component.ts
+++ angular-ui/src/app/component/other-masters/other-masters.component.ts
@@ -3,6 +3,7 @@ import { MatSidenav } from '@angular/material/sidenav';
 import {AfterViewInit, ViewChild} from '@angular/core';
 import { Sort, MatSort, SortDirection } from '@angular/material/sort';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-other-masters',
@@ -16,6 +17,8 @@ export class OtherMastersComponent implements OnInit {
 
   @ViewChild(MatSort)
   sort: MatSort = new MatSort;
+  currentPageName:string ="";
+  tabList:string[] = [];
 
   ngAfterViewInit() {
     //this.dataSource.sort = this.sort;
@@ -30,7 +33,36 @@ export class OtherMastersComponent implements OnInit {
     });
   }
   constructor(
-    private observer: BreakpointObserver,) { }
+    private observer: BreakpointObserver, private route: ActivatedRoute) {
+
+      this.route.data.subscribe(v => {
+        this.currentPageName = v['key'];
+        
+        // let actionColumn = v['usersMenuList'];
+        let showCaseTabList: string[] = [];
+        let userRolesData = JSON.parse(localStorage.getItem('userroles') ?? '[]');
+  
+        userRolesData.forEach(element => {
+          if (element.title == this.currentPageName) {
+            // this.columnDefs = this.columnDefs.filter(x => {
+            //   if (x.colId != 'action' || element == undefined || element == null) return true;
+  
+              element.permission.forEach(item => {
+                if(item.status){
+                  this.tabList.push(item.action.toLowerCase());
+                }
+                // if (actionColumn.indexOf(item.action.toLowerCase()) !== -1 && item.status) {
+                //   showCaseMenuList.push(item.action);
+                // }
+              // })
+              // return showCaseMenuList.length !== 0;
+            });
+          }
+        })
+        // console.log("showCaseMenuList.length", showCaseMenuList.length);
+        
+      })
+     }
 
   ngOnInit(): void {
  

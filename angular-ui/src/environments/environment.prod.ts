1a2424caf06ede7aee269686a300281d1c0e049c
Manikanta DK <manikantadk24@gmail.com>
2023-02-01 08:34:40 +0530

    changes

--- angular-ui/src/environments/environment.prod.ts
+++ angular-ui/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // url: 'http://52.172.24.161:801/api/',
 //  url: 'http://65.0.19.233:801/api/',
-//  url: 'http://13.126.235.145:801/api/',
-url: 'http://65.0.19.233:801/api/',
+ url: 'http://13.126.235.145:801/api/',
+
 };

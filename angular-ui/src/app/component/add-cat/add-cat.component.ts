9881571da1293c89f11f07534af6a1e30e6910c6
Teja <krishna.teja@smartgig.tech>
2023-02-28 00:00:55 +0530

    Add and edit category header positon updated

--- angular-ui/src/app/component/add-cat/add-cat.component.ts
+++ angular-ui/src/app/component/add-cat/add-cat.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { UserService } from 'src/app/services/user.service';
 // import { RestPwsdUserPopupComponent } from '../rest-pwsd-user-popup/rest-pwsd-user-popup.component';
-import {FormControl, FormGroupDirective, NgForm, Validators} from 
-             '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators } from
+  '@angular/forms';
 import { RestPwsdUserPopupComponent } from '../users/userPopups/rest-pwsd-user-popup/rest-pwsd-user-popup.component';
 import { ClassificationserviseService } from 'src/app/services/classificationservise.service';
 import { SharedService } from 'src/app/services/shared-services.service';
@@ -15,48 +15,48 @@ import { MaterialclassificationEditSuccessComponent } from './materialclassifica
   styleUrls: ['./add-cat.component.css']
 })
 export class AddCatComponent implements OnInit {
-  enterfirst:any;
-  entersecond:any;
-  userId:any;
-  error:any='';
-  LoginId:any;
-  addcat:any;
-  addcatcode:any;
-  numberValue:any;
-  catsetName:any;
-  activeCatId:any;
+  enterfirst: any;
+  entersecond: any;
+  userId: any;
+  error: any = '';
+  LoginId: any;
+  addcat: any;
+  addcatcode: any;
+  numberValue: any;
+  catsetName: any;
+  activeCatId: any;
   errorMsg: any;
 
-  adminPassword:boolean =false;
+  adminPassword: boolean = false;
   showPassword: boolean = false;
-  constructor( private dialog: MatDialog,
+  constructor(private dialog: MatDialog,
     private dialogRef: MatDialogRef<any>,
-    private user:UserService,
-    private calssification:ClassificationserviseService,
+    private user: UserService,
+    private calssification: ClassificationserviseService,
     private sharedService: SharedService,
 
 
-    ) { 
+  ) {
 
-   
-    }
 
-  
+  }
+
+
 
   ngOnInit(): void {
-    this.LoginId=localStorage.getItem("logInId");
+    this.LoginId = localStorage.getItem("logInId");
     this.numberValue = Number(this.LoginId);
     this.userId = localStorage.getItem("userID");
-    this.LoginId=localStorage.getItem("logInId");
-    this.activeCatId=localStorage.getItem("activeCatId");
-    this.catsetName=localStorage.getItem("catsetName");
+    this.LoginId = localStorage.getItem("logInId");
+    this.activeCatId = localStorage.getItem("activeCatId");
+    this.catsetName = localStorage.getItem("catsetName");
 
 
-    if(this.catsetName=='Edit Category'){
-this.calssification.getCatDetailsById(this.activeCatId).subscribe((res)=>{
-this.addcat=res.response.categoryName;
-this.addcatcode=res.response.categoryCode;
-})
+    if (this.catsetName == 'Edit Category') {
+      this.calssification.getCatDetailsById(this.activeCatId).subscribe((res) => {
+        this.addcat = res.response.categoryName;
+        this.addcatcode = res.response.categoryCode;
+      })
 
     }
 
@@ -65,69 +65,69 @@ this.addcatcode=res.response.categoryCode;
 
 
   }
- 
 
-  AddCat(){
-    let data={
-      CategoryName:this.addcat,
-      CategoryCode:this.addcatcode,
-      CreatedById:this.numberValue
-  };
-  this.calssification.addCatagory(data).subscribe((res)=>{
-    // this.sharedService.filter('Register click')
-    // this.addcat='';
-    // this.addcatcode='';
-    // this.dialogRef.close();
-    if (res.response.result === 'Success') {
-      sessionStorage.setItem("CategoryName",this.addcat);
-      sessionStorage.setItem("CategoryCode",this.addcatcode);
-      sessionStorage.setItem("subCategoryName",'');
-      sessionStorage.setItem("subCategoryCode",'');
-      sessionStorage.setItem("typeName",'');
-      sessionStorage.setItem("typeCode",'');
-      this.sharedService.filter('Register click')
-      this.addcat='';
-      this.addcatcode='';
-      this.dialogRef.close();
-      this.dialog.open(MaterialClassificationStatusPopupComponent, {panelClass: 'activeSuccessPop'});
+
+  AddCat() {
+    let data = {
+      CategoryName: this.addcat,
+      CategoryCode: this.addcatcode,
+      CreatedById: this.numberValue
+    };
+    this.calssification.addCatagory(data).subscribe((res) => {
+      // this.sharedService.filter('Register click')
+      // this.addcat='';
+      // this.addcatcode='';
+      // this.dialogRef.close();
+      if (res.response.result === 'Success') {
+        sessionStorage.setItem("CategoryName", this.addcat);
+        sessionStorage.setItem("CategoryCode", this.addcatcode);
+        sessionStorage.setItem("subCategoryName", '');
+        sessionStorage.setItem("subCategoryCode", '');
+        sessionStorage.setItem("typeName", '');
+        sessionStorage.setItem("typeCode", '');
+        this.sharedService.filter('Register click')
+        this.addcat = '';
+        this.addcatcode = '';
+        this.dialogRef.close();
+        this.dialog.open(MaterialClassificationStatusPopupComponent, { panelClass: 'activeSuccessPop' });
 
       }
-    else{
-      this.errorMsg=res.response.result;
-    }
+      else {
+        this.errorMsg = res.response.result;
+      }
 
-  })
+    })
 
   }
 
-  editCat(){
-    sessionStorage.setItem("CategoryName",this.addcat);
-      sessionStorage.setItem("CategoryCode",this.addcatcode);
-      sessionStorage.setItem("subCategoryName",'');
-      sessionStorage.setItem("subCategoryCode",'');
-      sessionStorage.setItem("typeName",'');
-      sessionStorage.setItem("typeCode",'');
-    const data={
-            CategoryId:this.activeCatId,
-            CategoryName:this.addcat,
-            CategoryCode:this.addcatcode,
-            LastModifiedById:this.LoginId
+  editCat() {
+    sessionStorage.setItem("CategoryName", this.addcat);
+    sessionStorage.setItem("CategoryCode", this.addcatcode);
+    sessionStorage.setItem("subCategoryName", '');
+    sessionStorage.setItem("subCategoryCode", '');
+    sessionStorage.setItem("typeName", '');
+    sessionStorage.setItem("typeCode", '');
+    const data = {
+      CategoryId: this.activeCatId,
+      CategoryName: this.addcat,
+      CategoryCode: this.addcatcode,
+      LastModifiedById: this.LoginId
 
     }
 
-    this.calssification.updateCat(data).subscribe((res)=>{
+    this.calssification.updateCat(data).subscribe((res) => {
       this.sharedService.filter('Register click')
-      this.addcat='';
-      this.addcatcode='';
+      this.addcat = '';
+      this.addcatcode = '';
       this.dialogRef.close();
-      this.dialog.open(MaterialclassificationEditSuccessComponent, {panelClass: 'activeSuccessPop'});
+      this.dialog.open(MaterialclassificationEditSuccessComponent, { panelClass: 'activeSuccessPop' });
     })
   }
 
 
-  close(){
+  close() {
     this.dialogRef.close();
 
   }
-  
+
 }

9eba6910a3a7c7f11bd2f45c859d712266f54dec
balaram <balaram.a@smartgig.tech>
2023-10-27 19:08:31 +0530

    Add-order-promotionlist calculation

--- angular-ui/src/app/component/order-list/order-list.component.ts
+++ angular-ui/src/app/component/order-list/order-list.component.ts
@@ -759,6 +759,7 @@ export class OrderListComponent implements OnInit {
   }
 
   addOrderPromotion() {
+    localStorage.removeItem('calculation')
     // 1 Promotion calculations
     localStorage.removeItem('FirstPromotionCalculation');
     localStorage.removeItem('FirstPromotionTotalAmountValue');
